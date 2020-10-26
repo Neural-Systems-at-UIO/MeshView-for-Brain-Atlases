b5b8a4b1368eb4f3fdf7ce0df23a2b97a2816ca2
Gergely Csucs <Tevemadar@users.noreply.github.com>
2020-10-26 16:32:09 +0100

    v0.8d minor cleanup
    - commented style, and code blocks from the past removed
    - PHP proxy removed

--- shaders.js
+++ shaders.js
@@ -31,12 +31,6 @@ function buildshaders(gl){
             "{"+
             "    gl_FragColor=color;"+
             "}"
-//            "precision highp float;"+
-//            "uniform vec3 color;"+
-//            "void main(void)"+
-//            "{"+
-//            "    gl_FragColor=vec4(color,1.);"+
-//            "}"
         ),
         cutcloud:buildprogram(gl,
             "attribute vec3 coords;"+
@@ -63,14 +57,6 @@ function buildshaders(gl){
             "    if(height>=0.)discard;"+
             "    gl_FragColor=color;"+
             "}"
-//            "precision highp float;"+
-//            "uniform vec3 color;"+
-//            "varying float height;"+
-//            "void main(void)"+
-//            "{"+
-//            "    if(height>=0.)discard;"+
-//            "    gl_FragColor=vec4(color,1.);"+
-//            "}"
         ),
         slicecloud:buildprogram(gl,
             "attribute vec3 coords;"+
@@ -101,14 +87,6 @@ function buildshaders(gl){
             "    if(height>=0. || height2<=0.)discard;"+
             "    gl_FragColor=color;"+
             "}"
-//            "precision highp float;"+
-//            "uniform vec3 color;"+
-//            "varying float height;"+
-//            "void main(void)"+
-//            "{"+
-//            "    if(height>=0.)discard;"+
-//            "    gl_FragColor=vec4(color,1.);"+
-//            "}"
         ),
         solidmesh:buildprogram(gl,
             "attribute vec3 coords;"+
@@ -185,182 +163,6 @@ function buildshaders(gl){
             "{"+
             "    if(height<=0.)discard;"+
             "    gl_FragColor=vec4(color,1.);"+
-            "}"
-        ),
-        cutmesh2:buildprogram(gl,
-            "attribute vec3 coords;"+
-            "attribute vec3 normals;"+
-
-            "uniform mat4 normtrf;"+
-            "uniform mat4 alltrf;"+
-            
-            "uniform vec3 cutbase;"+
-            "uniform vec3 cutnormal;"+
-            "uniform vec3 cutbase2;"+
-            "uniform vec3 cutnormal2;"+
-
-            "varying vec4 norm4;"+
-            "varying float height;"+
-            "varying float height2;"+
-            "void main(void)"+
-            "{"+
-            "    norm4=vec4(normals,1.)*normtrf;"+
-            "    height=dot(cutbase-coords,cutnormal);"+
-            "    height2=dot(cutbase2-coords,cutnormal2);"+
-            "    gl_Position = vec4(coords, 1.)*alltrf;"+
-            "}",
-
-            "precision highp float;"+
-            "uniform vec3 color;"+
-            "varying vec4 norm4;"+
-            "varying float height;"+
-            "varying float height2;"+
-            "void main(void)"+
-            "{"+
-  //          "    if(height<=0. || height2<=0.)discard;"+
-            "    if(height<=0. && height2<=0.)discard;"+
-            "    float n=abs(normalize(norm4).z);"+
-            "    gl_FragColor=vec4(color*n,1.);"+
-            "}"
-        ),
-        cutsurface2:buildprogram(gl,
-            "attribute vec3 coords;"+
-
-            "uniform mat4 alltrf;"+
-            
-            "uniform vec3 cutbase;"+
-            "uniform vec3 cutnormal;"+
-            "uniform vec3 cutbase2;"+
-            "uniform vec3 cutnormal2;"+
-
-            "varying float height;"+
-            "varying float height2;"+
-            "void main(void)"+
-            "{"+
-            "    height=dot(cutbase-coords,cutnormal);"+
-            "    height2=dot(cutbase2-coords,cutnormal2);"+
-            "    gl_Position = vec4(coords, 1.)*alltrf;"+
-            "}",
-
-            "precision highp float;"+
-            "uniform vec3 color;"+
-            "varying float height;"+
-            "varying float height2;"+
-            "void main(void)"+
-            "{"+
-//            "    if(height<=0. || height2<=0.)discard;"+
-            "    if(height<=0. && height2<=0.)discard;"+
-            "    gl_FragColor=vec4(color,1.);"+
-            "}"
-        ),
-        cutmesh3:buildprogram(gl,
-            "attribute vec3 coords;"+
-            "attribute vec3 normals;"+
-
-            "uniform mat4 normtrf;"+
-            "uniform mat4 alltrf;"+
-            
-            "uniform vec3 cutbase;"+
-            "uniform vec3 cutnormal;"+
-            "uniform vec3 cutbase2;"+
-            "uniform vec3 cutnormal2;"+
-            "uniform vec3 cutbase3;"+
-            "uniform vec3 cutnormal3;"+
-
-            "varying vec4 norm4;"+
-            "varying float height;"+
-            "varying float height2;"+
-            "varying float height3;"+
-            "void main(void)"+
-            "{"+
-            "    norm4=vec4(normals,1.)*normtrf;"+
-            "    height=dot(cutbase-coords,cutnormal);"+
-            "    height2=dot(cutbase2-coords,cutnormal2);"+
-            "    height3=dot(cutbase3-coords,cutnormal3);"+
-            "    gl_Position = vec4(coords, 1.)*alltrf;"+
-            "}",
-
-            "precision highp float;"+
-            "uniform vec3 color;"+
-            "varying vec4 norm4;"+
-            "varying float height;"+
-            "varying float height2;"+
-            "varying float height3;"+
-            "void main(void)"+
-            "{"+
-//            "    if(height<=0. || height2<=0. || height3<=0.)discard;"+
-            "    if(height<=0. && height2<=0. && height3<=0.)discard;"+
-            "    float n=abs(normalize(norm4).z);"+
-            "    gl_FragColor=vec4(color*n,1.);"+
-            "}"
-        ),
-        cutsurface3:buildprogram(gl,
-            "attribute vec3 coords;"+
-
-            "uniform mat4 alltrf;"+
-            
-            "uniform vec3 cutbase;"+
-            "uniform vec3 cutnormal;"+
-            "uniform vec3 cutbase2;"+
-            "uniform vec3 cutnormal2;"+
-            "uniform vec3 cutbase3;"+
-            "uniform vec3 cutnormal3;"+
-
-            "varying float height;"+
-            "varying float height2;"+
-            "varying float height3;"+
-            "void main(void)"+
-            "{"+
-            "    height=dot(cutbase-coords,cutnormal);"+
-            "    height2=dot(cutbase2-coords,cutnormal2);"+
-            "    height3=dot(cutbase3-coords,cutnormal3);"+
-            "    gl_Position = vec4(coords, 1.)*alltrf;"+
-            "}",
-
-            "precision highp float;"+
-            "uniform vec3 color;"+
-            "varying float height;"+
-            "varying float height2;"+
-            "varying float height3;"+
-            "void main(void)"+
-            "{"+
-//            "    if(height<=0. || height2<=0. || height3<=0.)discard;"+
-            "    if(height<=0. && height2<=0. && height3<=0.)discard;"+
-            "    gl_FragColor=vec4(color,1.);"+
-            "}"
-        ),
-        ghostmesh:buildprogram(gl,
-            "attribute vec3 coords;"+
-            "attribute vec3 normals;"+
-
-            "uniform mat4 normtrf;"+
-            "uniform mat4 alltrf;"+
-
-            "varying vec4 norm4;"+
-            "void main(void)"+
-            "{"+
-            "    norm4=vec4(normals,1.)*normtrf;"+
-            "    gl_Position = vec4(coords, 1.)*alltrf;"+
-            "}",
-
-            "precision highp float;"+
-            "uniform vec3 color;"+
-            "varying vec4 norm4;"+
-            "void main(void)"+
-            "{"+
-            "    float n=abs(normalize(norm4).z);"+
-//            "    float n=max(normalize(norm4).z,0.);"+
-//            "    gl_FragColor=vec4(color*n*(1.-n),1.-n);"+
-//            "    gl_FragColor=vec4(color*n,1.-n);"+
-//            "    gl_FragColor=vec4(color*n,1.-n);"+
-//            "    gl_FragColor=vec4(color*n*0.3,1.-n);"+
-//            "    gl_FragColor=vec4(color*n*0.3,(1.-n)*n);"+
-//            "    gl_FragColor=vec4(color*n*0.3,(1.-n)*(1.-n));"+
-//            "    gl_FragColor=vec4(color*(1.-n),1.-n);"+
-//            "    gl_FragColor=vec4(color*(1.-n)*0.5,1.-n);"+
-            "    gl_FragColor=vec4(color*n,1.-n);"+
-//            "    float n=pow(1.-abs(normalize(norm4).z),2.);"+
-//            "    gl_FragColor=vec4(color*(1.-n)*0.3,n);"+
             "}"
         ),
         transparentmesh:buildprogram(gl,
